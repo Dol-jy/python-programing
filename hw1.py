19bd6f487ec6b67c9e0fefa5cbc64944017174ee
dol_jy <tjrwndyd1@naver.com>
2024-03-20 20:19:42 +0900

    final

--- hw1.py
+++ hw1.py
@@ -13,7 +13,7 @@ def get_circle_area():
     return (v)
 
 
-radius = get_radius("넓이를 구하고자 하는 원의 반지름은? ")
+radius = get_radius('넓이를 구하고자 하는 원의 반지름은?')
 area = get_circle_area()
 print('반지름',radius, '인 원의 넓이 = 3.14 x',radius,'x',radius,'=', area)
 
